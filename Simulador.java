ae494349abdd3707144fa5ec270b0d9254037371
Rafael Acevedo <raa7@cin.ufpe.br>
2014-11-12 10:58:53 -0300

    melhorando script python

--- Simulador.java
+++ Simulador.java
@@ -5,40 +5,11 @@ import java.io.IOException;
 import java.util.Random;
 
 public class Simulador {
-	static final int QNTTAGS = 1000, QNTSLOTS = 64;
-	static int qntTags = QNTTAGS, qntSlots = QNTSLOTS, qntColisoes = 0,
+	static final int QNTSLOTS = 64;
+	static int qntTags, qntSlots = QNTSLOTS, qntColisoes = 0,
 			qntSucessos = 0, qntVazios = 0, qntTotais = qntSlots, qntTotaisColisoes = 0;
 	static int[] slots = new int[qntSlots];
 	static boolean cont = true;
-
-	public static void main(String[] args){
-		File f = new File("schoute"+QNTTAGS+".csv");
-		int contador = 1;
-		StringBuffer impressao = new StringBuffer();
-		impressao.append("SLOTS_TOTAIS,SLOTS_VAZIOS,SLOTS_COLISAO\r\n");
-		while (contador <= 1000) {
-			while (cont) {
-				leitura();
-			}
-			/*System.out.println("Simulação " + contador + ":"
-					+ "\nSlots totais: " + qntTotais + "\nSlots vazios: "
-					+ qntVazios + "\nSlots colisões: " + qntTotaisColisoes
-					+ "\n");*/
-			String toAppend = qntTotais + "," + qntVazios + "," + qntTotaisColisoes + "\r\n";
-			impressao.append(toAppend);
-			contador++;
-			qntTags = QNTTAGS;
-			qntSlots = qntTotais = QNTSLOTS;
-			qntColisoes = qntSucessos = qntVazios = qntTotaisColisoes = 0;
-			cont = true;
-		}
-		try {
-			gravar(impressao, f);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
 
 	public static void leitura() {
 		Random random = new Random();
@@ -46,7 +17,6 @@ public class Simulador {
 			int a = random.nextInt(qntSlots);
 			slots[a]++;
 		}
-
 		for (int i = 0; i < qntSlots; i++) {
 			if (slots[i] == 0) {
 				qntVazios++;
@@ -71,13 +41,10 @@ public class Simulador {
 			cont = false;
 		}
 	}
-	private static int schoute(){
-		return (int) Math.ceil(2.39 * qntColisoes);
-	}
+	
 	private static void gravar(StringBuffer impressao, File f) throws IOException {
 		BufferedWriter bw = null;
 		FileWriter fw = null;
-
 		try {
 			fw = new FileWriter(f);
 			bw = new BufferedWriter(fw);
@@ -87,6 +54,35 @@ public class Simulador {
 		} finally {
 			bw.close();
 			fw.close();
+		}
+	}
+	
+	private static int schoute(){
+		return (int) Math.ceil(2.39 * qntColisoes);
+	}
+	
+	public static void main(String[] args){
+		for(int qnt_tags = 100;qnt_tags<=1001;qnt_tags+=100){
+			File f = new File("schoute"+qnt_tags+".csv");
+			int contador = 1;
+			StringBuffer impressao = new StringBuffer();
+			while (contador <= 1000) {
+				while (cont) {
+					leitura();
+				}
+				String toAppend = qntTotais + "," + qntVazios + "," + qntTotaisColisoes + "\r\n";
+				impressao.append(toAppend);
+				contador++;
+				qntTags = qnt_tags;
+				qntSlots = qntTotais = QNTSLOTS;
+				qntColisoes = qntSucessos = qntVazios = qntTotaisColisoes = 0;
+				cont = true;
+			}
+			try {
+				gravar(impressao, f);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
--- generatechart.py
+++ generatechart.py
@@ -6,30 +6,22 @@ import matplotlib.pyplot as plt
 
 def plot(medida,titulo,nome):
 	fig = plt.figure()
-	ax1 = fig.add_subplot(1,1,1,axisbg='white')
+	ax = fig.add_subplot(1,1,1,axisbg='white')
 	plt.plot(tags_all,medida)
 	plt.title(titulo)
 	plt.ylabel('Slots')
 	plt.xlabel('Etiquetas')
 	plt.savefig(nome+'.png')
 tags_all = [100,200,300,400,500,600,700,800,900,1000]
-cnt = 0
 media_totais = []
 media_vazios = []
 media_colisao = []
-dp_totais = []
-dp_vazios = []
-dp_colisao = []
 for tags in tags_all:
 	totais,vazios,colisao = np.loadtxt('schoute'+str(tags)+'.csv',delimiter=',',unpack=True)
 	media_totais.append(np.mean(totais))
 	media_vazios.append(np.mean(vazios))
 	media_colisao.append(np.mean(colisao))
-	dp_totais.append(np.std(totais))
-	dp_vazios.append(np.std(vazios))
-	dp_colisao.append(np.std(colisao))
-	cnt = cnt + 1
 
-plot(media_totais, 'Slots totais', 'totais')
-plot(media_colisao, 'Slots em colisao', 'colisao')
-plot(media_vazios, 'Slots vazios', 'vazios')
+plot(media_totais, dp_totais, 'Slots totais', 'totais')
+plot(media_colisao, dp_colisao, 'Slots em colisao', 'colisao')
+plot(media_vazios, dp_vazios, 'Slots vazios', 'vazios')
